2a835e50b4b127abfdf3def3cb10665f80cd48b0
Dritriy Golyshev <4dimariK@gmail.com>
2023-09-09 13:05:53 +0300

    feat: dynamic cashe

--- public/sw.js
+++ public/sw.js
@@ -1,26 +1,60 @@
-const staticCacheName = 'static-site';
+const staticCacheName = 'static-site-v3';
+const dynamicCacheName = 'dynamic-site-v3';
 
 const ASSETS = [
   '/',
-  '/index.html',
   '/assets/icons/favicon-32x32.png',
   '/assets/Rick-And-Morty-Logo.png',
   '/assets/icons/android-chrome-192x192.png',
+  '/manifest.webmanifest',
   '/sw.js',
   '/app.js',
 ];
 
 self.addEventListener('install', async (e) => {
-  console.log('#### SW has been Installed');
   const cache = await caches.open(staticCacheName);
-  console.log('####: caches add ASSETS');
-  await cache.addAll(ASSETS);
+  try {
+    await cache.addAll(ASSETS);
+  } catch (error) {
+    console.error('####: staticCache', error);
+  }
 });
 
-self.addEventListener('activate', (e) => {
-  console.log('#### SW has been activated');
+self.addEventListener('activate', async (e) => {
+  const cachesKeysArr = await caches.keys();
+  await Promise.all(
+    cachesKeysArr
+      .filter((key) => key !== staticCacheName && key !== dynamicCacheName)
+      .map((key) => caches.delete(key))
+  );
 });
 
 self.addEventListener('fetch', (e) => {
-  console.log('#### fetch', e.request.url);
+  e.respondWith(cacheFirst(e.request));
 });
+
+async function cacheFirst(request) {
+  const cached = await caches.match(request);
+  try {
+    return (
+      cached ??
+      (await fetch(request).then((response) => {
+        return networkFirst(request);
+      }))
+    );
+  } catch (error) {
+    return networkFirst(request);
+  }
+}
+
+async function networkFirst(request) {
+  const cache = await caches.open(dynamicCacheName);
+  try {
+    const response = await fetch(request);
+    await cache.put(request, response.clone());
+    return response;
+  } catch (error) {
+    const cached = await cache.match(request);
+    return cached ?? (await cached.match('/404'));
+  }
+}
